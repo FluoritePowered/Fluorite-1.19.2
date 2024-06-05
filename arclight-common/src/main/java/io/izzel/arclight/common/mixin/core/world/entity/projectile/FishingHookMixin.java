5a006100801fe0395f1c536c369fa18bba631d4c
Kotori <79943751@qq.com>
2024-06-06 02:06:18 +0800

    Added missing LootContextParams#KILLER_ENTITY/THIS_ENTITY builder fixed LootJS#FISHING event cannot be called

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/entity/projectile/FishingHookMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/entity/projectile/FishingHookMixin.java
@@ -21,8 +21,6 @@ import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.HitResult;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.ItemFishedEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.FishHook;
 import org.bukkit.event.player.PlayerFishEvent;
@@ -34,7 +32,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.List;
 
@@ -42,33 +42,29 @@ import java.util.List;
 public abstract class FishingHookMixin extends ProjectileMixin {
 
     // @formatter:off
-    @Shadow public Entity hookedIn;
+    @Shadow @Nullable public abstract Player getPlayerOwner();
     @Shadow private int nibble;
-    @Shadow @Final private int luck;
-    @Shadow public abstract Player getPlayerOwner();
     @Shadow private int timeUntilHooked;
     @Shadow private int timeUntilLured;
     @Shadow @Final private int lureSpeed;
-    @Shadow protected abstract void pullEntity(Entity p_150156_);
-    // @formatter:on
-
-    public int minWaitTime = 100;
+    @Shadow@Nullable public Entity hookedIn;@Shadow protected abstract boolean shouldStopFishing(Player p_37137_);@Shadow public abstract void pullEntity(Entity p_150156_);@Shadow@Final private int luck;public int minWaitTime = 100;
     public int maxWaitTime = 600;
     public boolean applyLure = true;
+    // @formatter:on
 
     @Redirect(method = "checkCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/FishingHook;onHit(Lnet/minecraft/world/phys/HitResult;)V"))
     private void arclight$collide(FishingHook fishingHook, HitResult hitResult) {
         this.preOnHit(hitResult);
     }
 
-    @Inject(method = "catchingFish", at = @At(value = "FIELD", shift = At.Shift.AFTER, ordinal = 0, target = "Lnet/minecraft/world/entity/projectile/FishingHook;timeUntilHooked:I"))
-    private void arclight$attemptFail(BlockPos blockPos, CallbackInfo ci) {
+    @Inject(method = "catchingFish", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/network/syncher/SynchedEntityData;set(Lnet/minecraft/network/syncher/EntityDataAccessor;Ljava/lang/Object;)V", ordinal = 0))
+    private void fluorite$callPlayerFishEvent(BlockPos p_37146_, CallbackInfo ci) {
         PlayerFishEvent event = new PlayerFishEvent(((ServerPlayerEntityBridge) this.getPlayerOwner()).bridge$getBukkitEntity(), null, (FishHook) this.getBukkitEntity(), PlayerFishEvent.State.FAILED_ATTEMPT);
         Bukkit.getPluginManager().callEvent(event);
     }
 
-    @Inject(method = "catchingFish", cancellable = true, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/FishingHook;playSound(Lnet/minecraft/sounds/SoundEvent;FF)V"))
-    private void arclight$fishBite(BlockPos blockPos, CallbackInfo ci) {
+    @Inject(method = "catchingFish", at = @At(value = "INVOKE", shift = At.Shift.BEFORE, target = "Lnet/minecraft/world/entity/projectile/FishingHook;playSound(Lnet/minecraft/sounds/SoundEvent;FF)V"), cancellable = true)
+    private void fluorite$callPlayerFishEvent_1(BlockPos p_37146_, CallbackInfo ci) {
         PlayerFishEvent event = new PlayerFishEvent(((ServerPlayerEntityBridge) this.getPlayerOwner()).bridge$getBukkitEntity(), null, (FishHook) this.getBukkitEntity(), PlayerFishEvent.State.BITE);
         Bukkit.getPluginManager().callEvent(event);
         if (event.isCancelled()) {
@@ -86,56 +82,64 @@ public abstract class FishingHookMixin extends ProjectileMixin {
 
     /**
      * @author IzzelAliz
+     * @author Kotori0629
      * @reason
      */
     @Overwrite
     public int retrieve(ItemStack stack) {
-        Player playerentity = this.getPlayerOwner();
-        if (!this.level.isClientSide && playerentity != null) {
+        Player player = this.getPlayerOwner();
+        if (!this.level.isClientSide && player != null && !this.shouldStopFishing(player)) {
             int i = 0;
-            ItemFishedEvent event = null;
+            net.minecraftforge.event.entity.player.ItemFishedEvent event = null;
             if (this.hookedIn != null) {
-                PlayerFishEvent fishEvent = new PlayerFishEvent(((ServerPlayerEntityBridge) playerentity).bridge$getBukkitEntity(), ((EntityBridge) this.hookedIn).bridge$getBukkitEntity(), (FishHook) this.getBukkitEntity(), PlayerFishEvent.State.CAUGHT_ENTITY);
-                Bukkit.getPluginManager().callEvent(fishEvent);
-                if (fishEvent.isCancelled()) {
+                // CraftBukkit start
+                PlayerFishEvent playerFishEvent = new PlayerFishEvent(((ServerPlayerEntityBridge) player).bridge$getBukkitEntity(), ((EntityBridge) this.hookedIn).bridge$getBukkitEntity(), (FishHook) this.getBukkitEntity(), PlayerFishEvent.State.CAUGHT_ENTITY);
+                Bukkit.getPluginManager().callEvent(playerFishEvent);
+                if (playerFishEvent.isCancelled()) {
                     return 0;
                 }
+                // CraftBukkit end
                 this.pullEntity(this.hookedIn);
-                CriteriaTriggers.FISHING_ROD_HOOKED.trigger((ServerPlayer) playerentity, stack, (FishingHook) (Object) this, Collections.emptyList());
+                CriteriaTriggers.FISHING_ROD_HOOKED.trigger((ServerPlayer) player, stack, (FishingHook) (Object) this, Collections.emptyList());
                 this.level.broadcastEntityEvent((FishingHook) (Object) this, (byte) 31);
                 i = this.hookedIn instanceof ItemEntity ? 3 : 5;
             } else if (this.nibble > 0) {
-                LootContext.Builder lootcontext$builder = (new LootContext.Builder((ServerLevel) this.level)).withParameter(LootContextParams.ORIGIN, this.position()).withParameter(LootContextParams.TOOL, stack).withParameter(LootContextParams.THIS_ENTITY, (FishingHook) (Object) this).withRandom(this.random).withLuck((float) this.luck + playerentity.getLuck());
+                LootContext.Builder lootcontext$builder = (new LootContext.Builder((ServerLevel) this.level)).withParameter(LootContextParams.ORIGIN, this.position()).withParameter(LootContextParams.TOOL, stack).withParameter(LootContextParams.THIS_ENTITY, (FishingHook) (Object) this).withRandom(this.random).withLuck((float) this.luck + player.getLuck());
+                lootcontext$builder.withParameter(LootContextParams.KILLER_ENTITY, this.getOwner()).withParameter(LootContextParams.THIS_ENTITY, (FishingHook) (Object) this);
                 LootTable loottable = this.level.getServer().getLootTables().get(BuiltInLootTables.FISHING);
                 List<ItemStack> list = loottable.getRandomItems(lootcontext$builder.create(LootContextParamSets.FISHING));
-                event = new ItemFishedEvent(list, this.onGround ? 2 : 1, (FishingHook) (Object) this);
-                MinecraftForge.EVENT_BUS.post(event);
+                event = new net.minecraftforge.event.entity.player.ItemFishedEvent(list, this.onGround ? 2 : 1, (FishingHook) (Object) this);
+                net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(event);
                 if (event.isCanceled()) {
                     this.discard();
                     return event.getRodDamage();
                 }
-                CriteriaTriggers.FISHING_ROD_HOOKED.trigger((ServerPlayer) playerentity, stack, (FishingHook) (Object) this, list);
+                CriteriaTriggers.FISHING_ROD_HOOKED.trigger((ServerPlayer) player, stack, (FishingHook) (Object) this, list);
 
                 for (ItemStack itemstack : list) {
                     ItemEntity itementity = new ItemEntity(this.level, this.getX(), this.getY(), this.getZ(), itemstack);
-                    PlayerFishEvent playerFishEvent = new PlayerFishEvent(((ServerPlayerEntityBridge) playerentity).bridge$getBukkitEntity(), ((EntityBridge) itementity).bridge$getBukkitEntity(), (FishHook) this.getBukkitEntity(), PlayerFishEvent.State.CAUGHT_FISH);
+                    // CraftBukkit start
+                    PlayerFishEvent playerFishEvent = new PlayerFishEvent(((ServerPlayerEntityBridge) player).bridge$getBukkitEntity(), ((EntityBridge) itementity).bridge$getBukkitEntity(), (FishHook) this.getBukkitEntity(), PlayerFishEvent.State.CAUGHT_FISH);
                     playerFishEvent.setExpToDrop(this.random.nextInt(6) + 1);
                     Bukkit.getPluginManager().callEvent(playerFishEvent);
 
                     if (playerFishEvent.isCancelled()) {
                         return 0;
                     }
-                    double d0 = playerentity.getX() - this.getX();
-                    double d1 = playerentity.getY() - this.getY();
-                    double d2 = playerentity.getZ() - this.getZ();
+                    // CraftBukkit end
+                    double d0 = player.getX() - this.getX();
+                    double d1 = player.getY() - this.getY();
+                    double d2 = player.getZ() - this.getZ();
                     double d3 = 0.1D;
                     itementity.setDeltaMovement(d0 * 0.1D, d1 * 0.1D + Math.sqrt(Math.sqrt(d0 * d0 + d1 * d1 + d2 * d2)) * 0.08D, d2 * 0.1D);
                     this.level.addFreshEntity(itementity);
+                    // CraftBukkit start - this.random.nextInt(6) + 1 -> playerFishEvent.getExpToDrop()
                     if (playerFishEvent.getExpToDrop() > 0) {
-                        playerentity.level.addFreshEntity(new ExperienceOrb(playerentity.level, playerentity.getX(), playerentity.getY() + 0.5D, playerentity.getZ() + 0.5D, playerFishEvent.getExpToDrop()));
+                        player.level.addFreshEntity(new ExperienceOrb(player.level, player.getX(), player.getY() + 0.5D, player.getZ() + 0.5D, playerFishEvent.getExpToDrop()));
                     }
+                    // CraftBukkit end
                     if (itemstack.is(ItemTags.FISHES)) {
-                        playerentity.awardStat(Stats.FISH_CAUGHT, 1);
+                        player.awardStat(Stats.FISH_CAUGHT, 1);
                     }
                 }
 
@@ -143,22 +147,26 @@ public abstract class FishingHookMixin extends ProjectileMixin {
             }
 
             if (this.onGround) {
-                PlayerFishEvent playerFishEvent = new PlayerFishEvent(((ServerPlayerEntityBridge) playerentity).bridge$getBukkitEntity(), null, (FishHook) this.getBukkitEntity(), PlayerFishEvent.State.IN_GROUND);
+                // CraftBukkit start
+                PlayerFishEvent playerFishEvent = new PlayerFishEvent(((ServerPlayerEntityBridge) player).bridge$getBukkitEntity(), null, (FishHook) this.getBukkitEntity(), PlayerFishEvent.State.IN_GROUND);
                 Bukkit.getPluginManager().callEvent(playerFishEvent);
 
                 if (playerFishEvent.isCancelled()) {
                     return 0;
                 }
+                // CraftBukkit end
                 i = 2;
             }
 
+            // CraftBukkit start
             if (i == 0) {
-                PlayerFishEvent playerFishEvent = new PlayerFishEvent(((ServerPlayerEntityBridge) playerentity).bridge$getBukkitEntity(), null, (FishHook) this.getBukkitEntity(), PlayerFishEvent.State.REEL_IN);
+                PlayerFishEvent playerFishEvent = new PlayerFishEvent(((ServerPlayerEntityBridge) player).bridge$getBukkitEntity(), null, (FishHook) this.getBukkitEntity(), PlayerFishEvent.State.REEL_IN);
                 Bukkit.getPluginManager().callEvent(playerFishEvent);
                 if (playerFishEvent.isCancelled()) {
                     return 0;
                 }
             }
+            // CraftBukkit end
 
             this.discard();
             return event == null ? i : event.getRodDamage();
